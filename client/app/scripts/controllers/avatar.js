a844b4d6c901ca468cae1fdf6a8aa82290053bb5
Amelia Mendicino <amelia.mendicino@gmail.com>
2016-11-05 21:05:57 -0600

    Got all of the body sprites icon names into an array! WooHoo!

--- client/app/scripts/controllers/avatar.js
+++ client/app/scripts/controllers/avatar.js
@@ -8,10 +8,16 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('AvatarCtrl', function () {
-  	var avatar;
+  .controller('AvatarCtrl', function (grunticonEmbedConfig) {
+    var avatar;
+    var bodies = [];
+    angular.forEach(grunticonEmbedConfig.gruntIcons, function(value, key) {
+        // Grab all of the body icons out of the grunticons
+        if(key.endsWith('_body')) {
+            this.push(key);
+        }
+    }, bodies);
 
     avatar = this;
-
-    avatar.sprite = 'icon-female-human_body';
+    avatar.sprite = bodies[0];
   });
