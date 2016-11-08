8d4439149bf5da7d282dd35945f46b5f09954e21
Amelia Mendicino <amelia.mendicino@gmail.com>
2016-11-07 23:04:57 -0700

    Just a commit to show how the sprite tag can be used and how the models can be updated and reflect on the sprite.

--- client/app/scripts/controllers/avatar.js
+++ client/app/scripts/controllers/avatar.js
@@ -8,8 +8,8 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('AvatarCtrl', function (grunticonEmbedConfig) {
-    var avatar;
+  .controller('AvatarCtrl', function (grunticonEmbedConfig, $interval) {
+    var avatar = this;
     var bodies = [];
     angular.forEach(grunticonEmbedConfig.gruntIcons, function(value, key) {
         // Grab all of the body icons out of the grunticons
@@ -18,6 +18,12 @@ angular.module('clientApp')
         }
     }, bodies);
 
-    avatar = this;
-    avatar.sprite = bodies[0];
+    avatar.sprite = {};
+    avatar.sprite.body = bodies[0];
+    var colors = ['red', 'purple', 'pink', 'blue', false];
+
+    $interval(function() {
+        avatar.sprite.hair = {'color' : colors[Math.floor(Math.random()*colors.length)]};
+        avatar.sprite.body = bodies[Math.floor(Math.random()*bodies.length)];
+    }, 5000);
   });
