dadf8b9915e42b9d160c23972db6bf89b431a779
Rebekah Mendicino <rebekah.mendicino@gmail.com>
2016-11-08 21:25:46 -0700

    Removed the  function in avatar.js and added the _avatar.scss file.

--- client/app/scripts/controllers/avatar.js
+++ client/app/scripts/controllers/avatar.js
@@ -8,22 +8,16 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('AvatarCtrl', function (grunticonEmbedConfig, $interval) {
+  .controller('AvatarCtrl', function (grunticonEmbedConfig) {
     var avatar = this;
-    var bodies = [];
+    avatar.bodies = [];
     angular.forEach(grunticonEmbedConfig.gruntIcons, function(value, key) {
         // Grab all of the body icons out of the grunticons
         if(key.endsWith('_body')) {
             this.push(key);
         }
-    }, bodies);
+    }, avatar.bodies);
 
     avatar.sprite = {};
-    avatar.sprite.body = bodies[0];
-    var colors = ['red', 'purple', 'pink', 'blue', false];
-
-    $interval(function() {
-        avatar.sprite.hair = {'color' : colors[Math.floor(Math.random()*colors.length)]};
-        avatar.sprite.body = bodies[Math.floor(Math.random()*bodies.length)];
-    }, 5000);
+    avatar.sprite.body = avatar.bodies[0];
   });
