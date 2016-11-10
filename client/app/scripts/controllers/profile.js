61eae63d4df9e3a628da95b95d43de2f1f98cb03
Rebekah Mendicino <rebekah.mendicino@gmail.com>
2016-11-09 20:19:36 -0700

    Got the avatar to load on the profile page!

--- client/app/scripts/controllers/profile.js
+++ client/app/scripts/controllers/profile.js
@@ -9,16 +9,16 @@
  */
 angular.module('clientApp')
   .controller('ProfileCtrl', function ($http, $location) {
+    var profile = this;
     // Verify that the user has created an avatar before they go to the profile page
     $http.get('/avatar').then(function(response) {
       if(response.data.error) {
         console.log(response.data.error);
         $location.path('/avatar');
       }
+
+      profile.avatar = response.data;
     });
-
-
-    var profile = this;
 
     var request = $http.get('/profile');
 
