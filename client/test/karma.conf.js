c9182eaa3cc0a194d89cc82235ec220465038faa
Amelia Mendicino <amelia.mendicino@gmail.com>
2016-11-22 10:38:38 -0700

    Turned out that socket.io-client didn't have a "main" in its bower settings which confused wiredep so it didn't include the js in the index.html. By overriding/creating the "main" in the main bower.json, wiredep was able to pull it in properly. So cool.

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/socket.io-client/socket.io.js',
       'bower_components/angular-socket-io/socket.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
